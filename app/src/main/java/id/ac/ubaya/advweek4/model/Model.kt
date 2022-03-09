63d27d0f7d547a15070258cba86042c74bb6e376
Augie <augie100801@gmail.com>
2022-03-09 18:25:05 +0700

    selesai materi kelas

---
+++ app/src/main/java/id/ac/ubaya/advweek4/model/Model.kt
@@ -0,0 +1,9 @@
+package id.ac.ubaya.advweek4.model
+
+data class Student(
+    var id:String?,
+    var name:String?,
+    var dob:String?,
+    var phone:String?,
+    var photoURL:String?
+)
---
+++ app/src/main/java/id/ac/ubaya/advweek4/view/StudentDetailFragment.kt
@@ -0,0 +1,45 @@
+package id.ac.ubaya.advweek4.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
+import id.ac.ubaya.advweek4.R
+import id.ac.ubaya.advweek4.viewmodel.DetailViewModel
+import id.ac.ubaya.advweek4.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.fragment_student_list.*
+import kotlinx.android.synthetic.main.student_list_item.view.*
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [StudentDetailFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class StudentDetailFragment : Fragment() {
+    private lateinit var viewModel: DetailViewModel
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_student_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        viewModel.fetch()
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+    }
+
+}
